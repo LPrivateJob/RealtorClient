3310a63b7776290268fa50326d53799e435d95e1
sundong <sundong@yingu.com>
2018-01-04 10:12:04 +0800

    Initial commit

---
+++ app/src/main/java/com/installment/realtor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.installment.realtor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
