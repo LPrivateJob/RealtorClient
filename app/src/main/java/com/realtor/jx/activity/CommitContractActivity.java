3ef5c710198f932b83a27e631fd5a326d78e3c28
Lewish <lewishstart@163.com>
2018-01-08 16:11:59 +0800

    提交代码，备份下

--- app/src/main/java/com/realtor/jx/activity/CommitContractActivity.java
+++ app/src/main/java/com/realtor/jx/activity/CommitContractActivity.java
@@ -1,10 +1,16 @@
 package com.realtor.jx.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.view.KeyEvent;
 import android.widget.Toast;
 
 import com.realtor.jx.R;
 import com.realtor.jx.base.BaseActivity;
+import com.realtor.jx.base.BaseFragment;
+import com.realtor.jx.entity.Constants;
 import com.realtor.jx.widget.CommitContractStepIndicator;
 import com.realtor.jx.widget.Header;
 
@@ -16,6 +22,13 @@ import com.realtor.jx.widget.Header;
 public class CommitContractActivity extends BaseActivity {
     private Header mHeader;
     private CommitContractStepIndicator mStepIndicator;
+    private FragmentManager mFragmentManager = getSupportFragmentManager();
+
+    public static void open(Activity act, CommitContractStepIndicator.STEP step) {
+        Intent intent = new Intent(act, CommitContractActivity.class);
+        intent.putExtra(Constants.EXTRA_ENUM, step);
+        act.startActivity(intent);
+    }
 
     @Override
     protected void initView(Bundle savedInstanceState) {
@@ -41,5 +54,46 @@ public class CommitContractActivity extends BaseActivity {
     @Override
     protected int getLayoutResource() {
         return R.layout.activity_commit_contract;
+    }
+
+    //添加fragment
+    protected void addFragment(int layoutId, BaseFragment fragment) {
+        if (fragment != null) {
+            if (mFragmentManager.findFragmentById(layoutId) != null) {
+                //之前添加过
+                mFragmentManager.beginTransaction()
+                        .replace(layoutId, fragment, fragment.getClass().getSimpleName())
+                        .addToBackStack(fragment.getClass().getSimpleName())
+                        .commitAllowingStateLoss();
+            } else {
+                //之前未添加过
+                mFragmentManager.beginTransaction()
+                        .add(layoutId, fragment, fragment.getClass().getSimpleName())
+                        .addToBackStack(fragment.getClass().getSimpleName())
+                        .commitAllowingStateLoss();
+            }
+
+        }
+    }
+
+    //移除fragment
+    protected void removeFragment() {
+        if (mFragmentManager.getBackStackEntryCount() > 1) {
+            mFragmentManager.popBackStack();
+        } else {
+            finish();
+        }
+    }
+
+    //返回键返回事件
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (KeyEvent.KEYCODE_BACK == keyCode) {
+            if (mFragmentManager.getBackStackEntryCount() == 1) {
+                finish();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ app/src/main/java/com/realtor/jx/entity/Constants.java
@@ -0,0 +1,25 @@
+package com.realtor.jx.entity;
+
+/**
+ * author: sundong
+ * created at 2018/1/8 16:01
+ */
+public class Constants {
+    public static final String EXTRA_TITLE = "key_title";
+    public static final String EXTRA_ENUM = "key_enum";
+    public static final String EXTRA_ID = "key_id";
+    public static final String EXTRA_TYPE = "key_type";
+    public static final String EXTRA_CONTENT = "key_content";
+    public static final String EXTRA_URL = "key_url";
+    public static final String EXTRA_FRAGMENT_TYPE = "key_fragment_type";
+    public static final String EXTRA_EXIT_ANIM = "key_exit_anim";
+    public static final String EXTRA_HOUR = "hour";
+    public static final String EXTRA_MONERY = "money";
+    public static final String EXTRA_LIST = "list";
+    public static final String EXTRA_BEAN = "bean";
+    public static final String EXTRA_BOOL = "boolean";
+    public static final String EXTRA_BEAN2 = "bean2";
+    public static final String EXTRA_CODE = "code";
+    public static final String EXTRA_PASSWORD = "password";
+    public static final String EXTRA_TXT = "key_txt";
+}
