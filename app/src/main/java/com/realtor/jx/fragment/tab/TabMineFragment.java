13cc3b19f260f25a5d24747d7a4b034e807a4329
Lewish <lewishstart@163.com>
2018-01-09 22:59:47 +0800

    “我的”Tab UI完成

---
+++ app/src/main/java/com/realtor/jx/fragment/tab/TabMineFragment.java
@@ -0,0 +1,26 @@
+package com.realtor.jx.fragment.tab;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.realtor.jx.R;
+import com.realtor.jx.base.BaseFragment;
+
+/**
+ * description: 我的
+ * autour: lewish
+ * created at: 2018/1/9 22:57
+*/
+
+public class TabMineFragment extends BaseFragment {
+    @Override
+    protected void initView(View rootView, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.fragment_tab_mine;
+    }
+
+}
