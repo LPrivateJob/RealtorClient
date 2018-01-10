5631ed368029be4cb8b3a23e8c2861f750bb6e7a
Lewish <lewishstart@163.com>
2018-01-10 14:15:49 +0800

    上传照片UI完成

--- app/src/main/java/com/realtor/jx/fragment/UploadPicFragment.java
+++ app/src/main/java/com/realtor/jx/fragment/UploadPicFragment.java
@@ -2,6 +2,7 @@ package com.realtor.jx.fragment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.realtor.jx.R;
 import com.realtor.jx.base.BaseFragment;
@@ -10,11 +11,27 @@ import com.realtor.jx.base.BaseFragment;
  * description: 上传照片UI
  * autour: lewish
  * created at: 2018/1/6 10:38
-*/
+ */
 public class UploadPicFragment extends BaseFragment {
+    private ImageView mIvLeasingContractPH;
+    private ImageView mIvAddLeasingContract;
+    private ImageView mIvIDCardPH;
+    private ImageView mIvAddIDCard;
+    private ImageView mIvPOCPH;
+    private ImageView mIvAddPOC;
+    private ImageView mIvAgencyContractPH;
+    private ImageView mIvAddAgencyContract;
+
     @Override
     protected void initView(View rootView, Bundle savedInstanceState) {
-
+        mIvLeasingContractPH = findViewById(R.id.mIvLeasingContractPH);
+        mIvAddLeasingContract = findViewById(R.id.mIvAddLeasingContract);
+        mIvIDCardPH = findViewById(R.id.mIvIDCardPH);
+        mIvAddIDCard = findViewById(R.id.mIvAddIDCard);
+        mIvPOCPH = findViewById(R.id.mIvPOCPH);
+        mIvAddPOC = findViewById(R.id.mIvAddPOC);
+        mIvAgencyContractPH = findViewById(R.id.mIvAgencyContractPH);
+        mIvAddAgencyContract = findViewById(R.id.mIvAddAgencyContract);
     }
 
     @Override
