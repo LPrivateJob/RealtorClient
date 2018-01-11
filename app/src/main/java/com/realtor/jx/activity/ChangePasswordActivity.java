426f0322fba4e6a4d1c648927b2768e7f8be181c
Lewish <lewishstart@163.com>
2018-01-11 18:35:49 +0800

    备份一下

---
+++ app/src/main/java/com/realtor/jx/activity/ChangePasswordActivity.java
@@ -0,0 +1,22 @@
+package com.realtor.jx.activity;
+
+import android.os.Bundle;
+
+import com.realtor.jx.R;
+import com.realtor.jx.base.BaseActivity;
+
+/**
+ * author: sundong
+ * created at 2018/1/11 18:25
+ */
+public class ChangePasswordActivity extends BaseActivity {
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutResrce() {
+        return R.layout.activity_change_password;
+    }
+}
