e9d1f08568dc89f7e2cd5ec451ec7506391d5bdb
Lewish <lewishstart@163.com>
2018-01-11 15:13:39 +0800

    待审核，待修改页面UI完成

---
+++ app/src/main/java/com/realtor/jx/activity/WaitReviewActivity.java
@@ -0,0 +1,22 @@
+package com.realtor.jx.activity;
+
+import android.os.Bundle;
+
+import com.realtor.jx.R;
+import com.realtor.jx.base.BaseActivity;
+
+/**
+ * author: sundong
+ * created at 2018/1/11 15:04
+ */
+public class WaitReviewActivity extends BaseActivity {
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_wait_review;
+    }
+}
