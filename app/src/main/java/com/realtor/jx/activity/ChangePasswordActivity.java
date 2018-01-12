ffcd442ac084bdf527bb8dfb06ae1379933cf3f6
Lewish <lewishstart@163.com>
2018-01-12 19:04:23 +0800

    修改密码页面UI完成

--- app/src/main/java/com/realtor/jx/activity/ChangePasswordActivity.java
+++ app/src/main/java/com/realtor/jx/activity/ChangePasswordActivity.java
@@ -1,22 +1,54 @@
 package com.realtor.jx.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.realtor.jx.R;
 import com.realtor.jx.base.BaseActivity;
+import com.realtor.jx.widget.CountDownView;
+import com.realtor.jx.widget.Header;
 
 /**
  * author: sundong
  * created at 2018/1/11 18:25
  */
 public class ChangePasswordActivity extends BaseActivity {
+    private Header mHeader;
+    private TextInputLayout mEtPhoneTextWrapper;
+    private TextInputLayout mEtWrapperVerifyCode;
+    private TextInputLayout mEtWraperNewPassword;
+    private EditText mEtPhone;
+    private EditText mEtVerifyCode;
+    private EditText mEtNewPassword;
+    private TextView mBtnConfirm;
+    private CountDownView mTvGetVerifyCode;
+
     @Override
     protected void initView(Bundle savedInstanceState) {
+        mHeader = findViewById(R.id.mHeader);
+        mEtPhoneTextWrapper = findViewById(R.id.mEtPhoneTextWrapper);
+        mEtWrapperVerifyCode = findViewById(R.id.mEtWrapperVerifyCode);
+        mEtWraperNewPassword = findViewById(R.id.mEtWraperNewPassword);
+        mEtPhone = findViewById(R.id.mEtPhone);
+        mEtNewPassword = findViewById(R.id.mEtNewPassword);
+        mEtVerifyCode = findViewById(R.id.mEtVerifyCode);
+        mTvGetVerifyCode = findViewById(R.id.mTvGetVerifyCode);
+        mBtnConfirm = findViewById(R.id.mBtnConfirm);
 
+        mTvGetVerifyCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTvGetVerifyCode.startTimer();
+            }
+        });
     }
 
     @Override
-    protected int getLayoutResrce() {
+    protected int getLayoutResource() {
         return R.layout.activity_change_password;
     }
+
 }
---
+++ app/src/main/java/com/realtor/jx/widget/CountDownView.java
@@ -0,0 +1,70 @@
+package com.realtor.jx.widget;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.util.AttributeSet;
+import android.view.Gravity;
+
+import com.realtor.jx.R;
+
+/**
+ * description: 获取验证码的倒计时View
+ * autour: lewish
+ * created at: 2018/1/12 17:12
+*/
+public class CountDownView extends android.support.v7.widget.AppCompatButton {
+    private CountDownTimer timer;
+    private static final int COUNTDOWN_TIMER = 60 * 1000;
+
+    public CountDownView(Context context) {
+        this(context, null, 0);
+    }
+
+    public CountDownView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CountDownView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setGravity(Gravity.CENTER);
+        setClickable(true);
+        initTimer();
+    }
+
+    private void initTimer() {
+        timer = new CountDownTimer(COUNTDOWN_TIMER, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                setText(String.format(getResources().getString(R.string
+                        .code_cutdown), millisUntilFinished / 1000 + ""));
+            }
+
+            @Override
+            public void onFinish() {
+                setClickable(true);
+                setText("重试");
+
+            }
+        };
+    }
+
+    public void startTimer() {
+        if (timer != null) {
+            setClickable(false);
+            timer.start();
+        }
+    }
+
+    public void startTimerVoice() {
+        if (timer != null) {
+            timer.start();
+        }
+    }
+
+    public void cancel() {
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
+}
