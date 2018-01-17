566ed0c2187c9b5076ee775b416b1a369bd18796
Lewish <lewishstart@163.com>
2018-01-17 20:48:15 +0800

    分期预览页面框架搭建完成

--- app/src/main/java/com/realtor/jx/activity/InstallmentPreviewActivity.java
+++ app/src/main/java/com/realtor/jx/activity/InstallmentPreviewActivity.java
@@ -2,6 +2,7 @@ package com.realtor.jx.activity;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,15 +16,23 @@ import com.realtor.jx.base.BaseActivity;
  */
 public class InstallmentPreviewActivity extends BaseActivity {
     private TextView mTvTitlePlatformRepayAmount;
+    private TextView mTvTitleRenterRepayAmount;
+
+    private ListView mListViewPlatform;
+    private ListView mListViewRenter;
     private TextView mBtnNext;
 
     @Override
     protected void initView(Bundle savedInstanceState) {
         mTvTitlePlatformRepayAmount = findViewById(R.id.mTvTitlePlatformRepayAmount);
+        mTvTitleRenterRepayAmount = findViewById(R.id.mTvTitleRenterRepayAmount);
+        mListViewPlatform = findViewById(R.id.mListViewPlatform);
+        mListViewRenter = findViewById(R.id.mListViewRenter);
+
+        mTvTitlePlatformRepayAmount.setText(Html.fromHtml(String.format("平台付款共计<font color=\"#d40000\">%s</font>元", 30000)));
+        mTvTitleRenterRepayAmount.setText(Html.fromHtml(String.format("租户应还共计<font color=\"#d40000\">%s</font>元", 30000)));
         mBtnNext = findViewById(R.id.mBtnNext);
 
-        String str1=String.format("平台付款共计:<font color=\"#d40000\">%s</font>元", 30000);
-        mTvTitlePlatformRepayAmount.setText(Html.fromHtml(str1));
     }
 
     @Override
