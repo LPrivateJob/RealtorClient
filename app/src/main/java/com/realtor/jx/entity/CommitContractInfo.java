619e02ae8e2f1ff330f198452c68d20eb6f1d69e
Lewish <lewishstart@163.com>
2018-01-20 15:34:47 +0800

    添加合同提交实体类

---
+++ app/src/main/java/com/realtor/jx/entity/CommitContractInfo.java
@@ -0,0 +1,27 @@
+package com.realtor.jx.entity;
+
+/**
+ * description: 提交合同类
+ * autour: lewish
+ * created at: 2018/1/20 15:23
+*/
+
+public class CommitContractInfo {
+    private String tenancyName;//姓名
+    private String tenancyMobile;//手机号码
+    private String tenancyIdcard;//身份证号
+    private String tenancyType;//租住方式
+    private String cityNo;//所在城市
+    private String houseName;//小区名称
+    private String houseCode;//门牌号
+    private String roomNum;//房间号
+    private String cash;//月租金
+    private String startTime;//起租日
+    private String endTime;//到租日
+    private String feeType;//服务费承担方
+    private String firstPaytype;//租客首付方式
+    private String platformPayType;//平台付款方式
+    private String payTerm;//还款期数
+    private String changeNo;//台账号
+    private String info;//备注信息
+}
